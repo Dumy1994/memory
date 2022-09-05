fd76d301d3c36d26dceee2f19bb4faf04bcd0163
dumy1994 <dumitru_budau@yahoo.it>
2022-09-05 11:28:18 +0200

    array check id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  // colors array 
+  const colors = [
+    {
+        id: 1,
+        name: 'red',
+        numero: 1,
+    },
+    {
+        id: 2,
+        name: 'blue',
+        numero: 2
+    },
+    {
+        id: 3,
+        name: 'green',
+        numero: 3
+    },
+    {
+        id: 4,
+        name: 'yellow',
+        numero: 4
+    },
+    {
+        id: 5,
+        name: 'orange',
+        numero: 5
+    },
+    {
+        id: 6,
+        name: 'purple',
+        numero: 6
+    },
+   
+]
+  // double array 
+  const colorsDouble = colors.concat(...colors)
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const BLANK_CARD = "https://progitek.no/privat/bp/wp-content/uploads/2021/09/pexels-pixabay-235985-scaled.jpg"
+  //  flip colors 
+  function flipColors(colors, id) {
+    console.log(colors, id)
+  }
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='griglia'>
+        {/* print card colors random  */}
+        {colorsDouble.sort(() => Math.random() - 0.5).map((color, id) => (
+          <div onClick={() => flipColors (color, id)} key={id} className='card-container'>
+            <div style={{backgroundColor: color.name }} className='card'>
+            </div>
+          </div>
+        ))}
+
+
+
+
+        
+        {/* print card numbers  */}
+        {/* <div className="absoulte">
+          {numbers.map((number, index) => (
+            <div key={index} className='card-container'>
+              <div style={{backgroundColor: 'white'}} className='card numero'>
+                {number}
+              </div>
+            </div>
+          ))}
+        </div> */}
+      </section>
     </div>
   );
 }
---
+++ src/colors.js
@@ -0,0 +1,34 @@
+const colors = [
+    {
+        id: 1,
+        name: 'red',
+        numero: 1,
+    },
+    {
+        id: 2,
+        name: 'blue',
+        numero: 2
+    },
+    {
+        id: 3,
+        name: 'green',
+        numero: 3
+    },
+    {
+        id: 4,
+        name: 'yellow',
+        numero: 4
+    },
+    {
+        id: 5,
+        name: 'orange',
+        numero: 5
+    },
+    {
+        id: 6,
+        name: 'purple',
+        numero: 6
+    },
+   
+]
+export default colors;
